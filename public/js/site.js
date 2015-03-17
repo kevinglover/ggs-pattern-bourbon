c2e306633ed1b5cdeab782cfeb576d615aa76892
Kevin Glover <hkglover@gmail.com>
2015-03-17 16:51:50 -0400

    Added peekaboo header - Shows full header at top (chapter and section) - Collapses on scroll at breakpoint - adding 'data-page="main"' will hide the chapter name at the top

--- public/js/site.js
+++ public/js/site.js
@@ -15,6 +15,38 @@
     };
     
     $('select').selectize(options);
+  });
+  
+  // Init Peakaboo 
+  $(function() {
+    
+    var $peakaboo = $("header.peakaboo"),
+        scrollBP = $peakaboo.offset().top + 100,
+        $mini = $peakaboo.clone().addClass('mini');
+        
+        $mini.insertAfter($peakaboo);
+    
+    $(window).on('scroll.peakaboo', function(){
+      
+      var scrollTop = $(this).scrollTop();
+      if (scrollTop > scrollBP){
+        $mini.css({'display':'block'});
+        $peakaboo.addClass('p-hidden');
+      }
+      else{
+        $mini.css({'display':'none'});
+        $peakaboo.removeClass('p-hidden');
+      }
+      
+      if (scrollTop > (scrollBP + $peakaboo.height()) ){
+        $mini.addClass('shadow');
+      }
+      else{
+        $mini.removeClass('shadow');
+      }
+      
+    });
     
   });
+  
 })(this);
