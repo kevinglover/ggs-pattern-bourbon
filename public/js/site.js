cc722d770e58c2ae7a6b02af82d222ea63afaac1
Kevin Glover <hkglover@gmail.com>
2015-03-17 09:41:21 -0400

    Cleaned up fonts and select styles

--- public/js/site.js
+++ public/js/site.js
@@ -1,7 +1,23 @@
 (function(){
   
+  // Load Fonts
+  // WebFontConfig = {
+  //   google: { families: [ 'Fira+Sans:300,400,500,700,400italic:latin' ] }
+  // };
+  // (function() {
+  //   var wf = document.createElement('script');
+  //   wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+  //     '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+  //   wf.type = 'text/javascript';
+  //   wf.async = 'true';
+  //   var s = document.getElementsByTagName('script')[0];
+  //   s.parentNode.insertBefore(wf, s);
+  // })();
+  
+  // Init Material 
   $.material.init();
   
+  // Init Selectize 
   $(function() {
     var options = {
       onDropdownOpen: function () {
